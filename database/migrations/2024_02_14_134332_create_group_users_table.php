2db93b68a3420d1981b0d8e45db8bb832fe64fd8
ahmedellithy99 <ahmedelithy99@gmail.com>
2024-02-14 16:15:39 +0200

    Add migrations for database schema

---
+++ database/migrations/2024_02_14_134332_create_group_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('status', 25);
+            $table->string('role', 25);
+            $table->string('token', 1024)->nullable();
+            $table->timestamp('token_expire_date')->nullable();
+            $table->timestamp('token_used')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('group_id')->constrained();
+            $table->foreignId('created_by')->constrained('users');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_users');
+    }
+};
